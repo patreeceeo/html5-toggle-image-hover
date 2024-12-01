8ca5bdab61da45f76d9a24d2693c2564ee083505
Patrick Canfield <pscale01@gmail.com>
2024-11-30 21:54:51 -0800

    use selector

--- jsMethod/index.js
+++ jsMethod/index.js
@@ -2,35 +2,37 @@
 setTimeout(main);
 
 const images = {
-  rocko: {
+  ".rocko": {
     default: "/rocko-1.jpg",
     hover: "/rocko-2.jpg",
   },
 }
 
 function main() {
-  for(const [id, srcs] of Object.entries(images)) {
-    const enterListener = swapImage(id, srcs.hover);
-    const leaveListener = swapImage(id, srcs.default);
-    const el = getElementById(id);
-    el.addEventListener("mouseenter", enterListener);
-    el.addEventListener("mouseleave", leaveListener);
-    leaveListener();
+  for(const [selector, srcs] of Object.entries(images)) {
+    const enterListener = swapImage(srcs.hover);
+    const leaveListener = swapImage(srcs.default);
+    const els = findElements(selector);
+    for(const el of els) {
+      el.addEventListener("mouseenter", enterListener);
+      el.addEventListener("mouseleave", leaveListener);
+      leaveListener({ target: el });
+    }
   }
 }
 
-function getElementById(id) {
-  const el = document.getElementById(id);
-  if(el) {
-    return el;
+function findElements(selector) {
+  const els = document.querySelectorAll(selector);
+  if(els.length > 0) {
+    return els;
   } else {
-    throw new Error(`Oh no! JavaScript tried to get an element with ID="${id}" but it doesn't exist in the DOM!`);
+    throw new Error(`Oh no! JavaScript tried to get an element with selector "${selector}" but it doesn't exist in the DOM!`);
   }
 }
 
-function swapImage(id, src) {
-  return () => {
-    const el = getElementById(id);
+function swapImage(src) {
+  return (event) => {
+    const el = event.target;
     el.src = src;
   }
 }
